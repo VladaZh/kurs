8e5b7c9234f82453052b01a603f5fc060dfeaf68
Vladislava Zhuikova <vladazhuikova@gmail.com>
2025-10-26 12:37:44 +0300

    data to the db

--- library/app/models.py
+++ library/app/models.py
@@ -5,7 +5,8 @@ class Book(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=50)
-    description = models.TextField
+    description = models.CharField(max_length=2000)
+    genre = models.CharField(max_length=20)
 
     def __str__(self):
         return self.title
