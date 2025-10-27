9bf8adaf552d10033e1111adc66c3fb8f7d0bfdf
Vladislava Zhuikova <vladazhuikova@gmail.com>
2025-10-27 13:32:18 +0300

    book in details

--- library/app/views.py
+++ library/app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Book
+from django.shortcuts import render, get_object_or_404
 
 def library(request):
     books = Book.objects.all()
@@ -15,3 +16,10 @@ def archive(request):
 
 def profile(request):
     return render(request, 'app/profile.html')
+
+def book_detail(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    context = {
+        'book': book
+    }
+    return render(request, 'app/book.html', context)
