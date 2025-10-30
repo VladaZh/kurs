e79d9e2f990ba9cd0ae7cb5acb64a2cd11f14d85
Vladislava Zhuikova <vladazhuikova@gmail.com>
2025-10-30 14:01:14 +0300

    path changed

--- library/library/urls.py
+++ library/library/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     path('article/<int:article_id>/', views.article_detail, name='article_detail'),
     path('sign-up/', views.sign_up_view, name='sign_up'),
     path('sign-in/', views.sign_in_view, name='sign_in'),  # отдельная страница входа
-    path('logout/', auth_views.LogoutView.as_view(next_page='library'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='profile'), name='logout'),
 ]
